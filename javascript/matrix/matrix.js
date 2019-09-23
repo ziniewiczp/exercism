a887c4f3fe1fd376ad367b8eea16e55b6ca6104c
ziniewiczp <Piotr.Ziniewicz@contracted.pmi.com>
2019-09-23 16:42:31 +0200

    Assign values for rows and columns directly by returning from map functions.

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,17 +1,6 @@
 export class Matrix {
     constructor(matrix) {
-        this.rows = new Array();
-        this.columns = new Array();
-        
-        matrix.split("\n").map(row => {
-            this.rows.push(row.split(" ").map(Number));
-
-            row.split(" ").map((item, index) => {
-                if(this.columns.length < index + 1)
-                    this.columns.push(new Array());
-
-                this.columns[index].push(+item);
-            });
-        });
+        this.rows = matrix.split("\n").map(row => row.split(" ").map(Number));
+        this.columns = this.rows[0].map((_, index) => this.rows.map(row => row[index]));
     }
 }
