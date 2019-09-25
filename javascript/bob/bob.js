78fc28ec89ff925b35c1eee52a52a6667d7e39b7
Piotr Ziniewicz <ziniewicz.p@gmail.com>
2019-09-25 16:38:40 +0200

    Refactor solution for Bob exercise.

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,18 +1,11 @@
 export const hey = (message) => {
   message = message.replace(/\s+/g, "");
 
-  if(message === "") {
+  if(message === "")
     return "Fine. Be that way!";
-  }
   
-  if(message === message.toUpperCase() && message !== message.replace(/[A-Za-z]/g, "")) {
-    if(message.endsWith("?")) {
-      return "Calm down, I know what I\'m doing!";
-
-    } else {
-      return "Whoa, chill out!";
-    }
-  }
+  if(message !== message.replace(/[A-Za-z]/g, "") && message === message.toUpperCase())
+    return (message.endsWith("?")) ? "Calm down, I know what I\'m doing!" : "Whoa, chill out!";
 
   if(message.endsWith("?"))
     return "Sure.";
