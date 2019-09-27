fd851f41aebec32616d7e10daaa66b8cd9a75a30
ziniewiczp <Piotr.Ziniewicz@contracted.pmi.com>
2019-09-27 13:12:42 +0200

    Move all the logic to the reduce function.

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -1,25 +1,14 @@
 const letterValues = new Map([
-  ["Z", 10],
-  ["Q", 10],
-  ["X", 8],
-  ["J", 8],
+  ["Z", 10], ["Q", 10],
+  ["X", 8], ["J", 8],
   ["K", 5],
-  ["F", 4],
-  ["H", 4],
-  ["V", 4],
-  ["W", 4],
-  ["Y", 4],
-  ["B", 3],
-  ["C", 3],
-  ["M", 3],
-  ["P", 3],
-  ["D", 2],
-  ["G", 2]
+  ["F", 4], ["H", 4], ["V", 4], ["W", 4], ["Y", 4],
+  ["B", 3], ["C", 3], ["M", 3], ["P", 3],
+  ["D", 2], ["G", 2]
 ]);
 
 export const score = (word) => {
-  return (word === "") ? 0 : word.split("").map(letter => {
-    return (letterValues.get(letter.toUpperCase())) ? letterValues.get(letter.toUpperCase()) : 1;
-  
-  }).reduce((sum, current) => sum += current);
+  return [...word.toUpperCase()].reduce((sum, current) => {
+    return sum + (letterValues.get(current) || 1);
+  }, 0);
 };
