60856dfc62cf86d3fb79d49c8041c375f8dbff67
ziniewiczp <Piotr.Ziniewicz@contracted.pmi.com>
2019-10-03 11:45:51 +0200

    Add solution for Pascal's Triangle exercise.

---
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -0,0 +1,18 @@
+export class Triangle {
+  constructor(height) {
+    this.rows = new Array();
+
+    for(let i = 0; i < height; i++) {
+      let row = new Array();
+      for(let j = 0; j < i + 1; j++) {
+        row.push((j !== 0 && j !== i) ? this.rows[i-1][j-1] + this.rows[i-1][j] : 1);
+      }
+
+      this.rows.push(row);
+    }
+  }
+
+  get lastRow() {
+    return this.rows.pop();
+  }
+}
