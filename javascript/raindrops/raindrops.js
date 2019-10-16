82e163d469d5484d3f04cbd841c1eeeac7441ae7
ziniewiczp <Piotr.Ziniewicz@contracted.pmi.com>
2019-10-16 15:27:53 +0200

    Add solution for Raindrops exercise.

---
+++ javascript/raindrops/raindrops.js
@@ -0,0 +1,8 @@
+export const convert = (number) => {
+  let result = "";
+  if(number % 3 === 0) result += "Pling";
+  if(number % 5 === 0) result += "Plang";
+  if(number % 7 === 0) result += "Plong";
+
+  return result || number.toString();
+};
