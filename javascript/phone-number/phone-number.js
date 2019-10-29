260a247d1213b91ea20761521d5dcb92b43059dd
ziniewiczp <Piotr.Ziniewicz@contracted.pmi.com>
2019-10-29 08:55:49 +0100

    Enhance the solution for Phone Number exercise.

--- javascript/phone-number/phone-number.js
+++ javascript/phone-number/phone-number.js
@@ -1,22 +1,14 @@
 export class PhoneNumber {
   constructor(input) {
-    let formattedNumber = input.split("").filter(current => { 
-      return current.match(/[0-9]/);
-    }).join("");
-
-    if(formattedNumber[0] === "1") formattedNumber = formattedNumber.substr(1);
-
-    if(formattedNumber.length === 10 
-        && formattedNumber[0] !== "1" 
-        && formattedNumber[0] !== "0" 
-        && formattedNumber[3] !== "1" 
-        && formattedNumber[3] !== "0") {
-      this.formattedNumber = formattedNumber;
-    
-    }
+    let digits = input.replace(/[^0-9]/g, "");
+    if(digits[0] === "1")
+      digits = digits.substr(1);
+      
+    if(digits.length === 10 && digits[0] > 1 && digits[3] > 1)
+      this.phoneNumber = digits;
   }
 
   number() {
-    return this.formattedNumber || null;
+    return this.phoneNumber || null;
   }
 }
