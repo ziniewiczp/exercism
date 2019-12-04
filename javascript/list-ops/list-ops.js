6a3e95f47ddb434121308e1cccb4d5436e076204
ziniewiczp <Piotr.Ziniewicz@contracted.pmi.com>
2019-12-04 12:10:47 +0100

    Replace while statements with foldl.

--- javascript/list-ops/list-ops.js
+++ javascript/list-ops/list-ops.js
@@ -1,78 +1,60 @@
 export class List {
     constructor(initialValues = new Array()) {
         this.values = initialValues;
-        return this;
     }
 
     append(newList) {   
-        this.values = [...this.values, ...newList.values];
-        return this;
+        return new List([...this.values, ...newList.values]);
     }
 
     concat(listOfLists) {
-        let [head, ...tail] = listOfLists.values;
-        while(head) {
-            this.append(head);
-            [head, ...tail] = tail;
-        }
-
-        return this;
+        return listOfLists.foldl((result, current) => {
+            return result.append(current)
+        }, new List(this.values));
     }
 
     filter(predicate) {
-        let [head, ...tail] = this.values;
-        this.values = new Array();
+        return this.foldl((filtered, current) => {
+            if(predicate(current)) { 
+                filtered.values.push(current); 
+            }
 
-        while(head) {
-            if(predicate(head)) { this.values.push(head); }
-            [head, ...tail] = tail;
-        }
-
-        return this;
+            return filtered
+        }, new List());
     }
 
     map(transformation) {
-        let [head, ...tail] = this.values;
-        this.values = new Array();
-        
-        while(head) {
-            this.values.push(transformation(head));
-            [head, ...tail] = tail;
-        }
-
-        return this;
+        return this.foldl((mapped, current) => {
+            mapped.values.push(transformation(current));
+            return mapped;
+        }, new List());
     }
 
-    length(counter = 0, rest = this.values) {
+    length(counter = 0, rest = [...this.values]) {
         const [head, ...tail] = rest;
         return (head)
             ? this.length(counter + 1, tail)
             : counter;
     }
 
-    foldl(transformation, accumulator, rest = this.values) {
+    foldl(transformation, accumulator, rest = [...this.values]) {
         const [head, ...tail] = rest;
         return (head) 
             ? this.foldl(transformation, transformation(accumulator, head), tail) 
             : accumulator;
     }
 
-    foldr(transformation, accumulator, rest = this.values) {
+    foldr(transformation, accumulator, rest = [...this.values]) {
         const last = rest.pop();
         return (last) 
             ? this.foldr(transformation, transformation(accumulator, last), rest) 
             : accumulator;
     }
 
-    reverse(temp = new Array()) {
-        const [head, ...tail] = this.values;
-        if(!head) { return this; }
-        
-        temp.push(head);
-        this.values = tail;
-        
-        this.reverse(tail, temp);
-        this.values.push(temp.pop());
-        return this;
+    reverse() {
+        return this.foldr((reversed, current) => {
+            reversed.values.push(current);
+            return reversed;
+        }, new List());
     }
 }
